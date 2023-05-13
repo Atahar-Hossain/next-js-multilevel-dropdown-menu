38c49464542a91cf79743c1f1bdb9a4457a49b84
Atahar-Hossain <ataharhossain247@gmail.com>
2023-05-13 20:14:56 +0600

    added

--- components/navbar.js
+++ components/navbar.js
@@ -2,20 +2,22 @@ import React from "react";
 import navbar from "./navbar.module.css";
 
 export default function Navbar() {
-	const menu_item = [
-		{ title: "Home" },
-		{ title: "Services" },
-		{ title: "Contact" },
-		{ title: "Technologies" },
-		{ title: "Hardware" },
-		{ title: "Software" },
-		{ title: "About" },
+	const menu_items = [
+		{ id: 1, title: "Home" },
+		{ id: 2, title: "Services" },
+		{ id: 3, title: "Contact" },
+		{ id: 4, title: "Technologies" },
+		{ id: 5, title: "Hardware" },
+		{ id: 6, title: "Software" },
+		{ id: 7, title: "About" },
 	];
 	return (
 		<div className={navbar.header}>
-			{
-				menu_item.map
-			}
+			<ul>
+				{menu_items.map((item) => {
+					return <li>{item.title}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
