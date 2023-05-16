3b162ce6a5b05e8839361cb92d6257b088d73dc3
Atahar-Hossain <ataharhossain247@gmail.com>
2023-05-17 01:37:37 +0600

    added new

--- components/menuItem.js
+++ components/menuItem.js
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import SubmenuDropdown from "./submenuDropdown";
 
 function MenuItem({ item, depth }) {
+	const [showSubmenu, setShowSubmenu] = useState(false);
+
+	function handleSubMenu() {
+		setShowSubmenu((prev) => !prev);
+		// console.log("clicked");
+	}
+
 	return item.submenu && item.submenu.length > 0 ? (
 		<li className="parentMenu">
-			<button id="submenuButton">
+			<button id="submenuButton" onClick={handleSubMenu}>
 				{item.title}
 
 				{depth > 1 ? (
@@ -13,7 +21,11 @@ function MenuItem({ item, depth }) {
 					<span className="arrowDown"></span>
 				)}
 			</button>
-			<SubmenuDropdown submenu={item.submenu} depth={depth} />
+			<SubmenuDropdown
+				submenu={item.submenu}
+				depth={depth}
+				showSubmenu={showSubmenu}
+			/>
 		</li>
 	) : (
 		<li>{item.title}</li>
--- components/submenuDropdown.js
+++ components/submenuDropdown.js
@@ -1,12 +1,17 @@
+// "use client";
 import React from "react";
 import MenuItem from "./menuItem";
 // // import styles from "./Menu.module.css";
 // import childMenu from "../app/globals.css";
 // import childMenuStyle from "../app/globals.css";
 
-function SubmenuDropdown({ submenu, depth }) {
+function SubmenuDropdown({ submenu, depth, showSubmenu }) {
 	return (
-		<ul className={`${"childMenu"} ${depth > 1 ? "childMenuStyle" : ""}`}>
+		<ul
+			className={`${"childMenu"} ${depth > 1 ? "childMenuStyle" : ""} ${
+				showSubmenu ? "showChildMenu" : "hideChildMenu"
+			}`}
+		>
 			{submenu.map((menu) => (
 				<MenuItem item={menu} key={submenu.id} depth={depth + 1} />
 			))}
